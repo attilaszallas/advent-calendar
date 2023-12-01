9170a0f6bee47d70e13eebde1e3243e6fece3c10
Attila Szallas <ASzallas@slb.com>
2023-12-01 13:00:04 +0100

    Add button logic

---
+++ advent.js
@@ -0,0 +1,132 @@
+var button3 = document.getElementById('btn-3');
+var button4 = document.getElementById('btn-4');
+var button5 = document.getElementById('btn-5');
+var button6 = document.getElementById('btn-6');
+var button7 = document.getElementById('btn-7');
+var button8 = document.getElementById('btn-8');
+var button9 = document.getElementById('btn-9');
+var button10 = document.getElementById('btn-10');
+var button11 = document.getElementById('btn-11');
+var button12 = document.getElementById('btn-12');
+var button13 = document.getElementById('btn-13');
+var button14 = document.getElementById('btn-14');
+var button15 = document.getElementById('btn-15');
+var button16 = document.getElementById('btn-16');
+var button17 = document.getElementById('btn-17');
+var button18 = document.getElementById('btn-18');
+var button19 = document.getElementById('btn-19');
+var button20 = document.getElementById('btn-20');
+var button21 = document.getElementById('btn-21');
+var button22 = document.getElementById('btn-22');
+var button23 = document.getElementById('btn-23');
+var button24 = document.getElementById('btn-24');
+button3 === null || button3 === void 0 ? void 0 : button3.addEventListener('click', function handleClick(event) {
+    console.log('button3 clicked');
+    button3.style.backgroundColor = "#000080";
+    button3.textContent = "Erfurt";
+});
+button4 === null || button4 === void 0 ? void 0 : button4.addEventListener('click', function handleClick(event) {
+    console.log('button4 clicked');
+    button4.style.backgroundColor = "#000080";
+    button4.textContent = "Bielefeld";
+});
+button5 === null || button5 === void 0 ? void 0 : button5.addEventListener('click', function handleClick(event) {
+    console.log('button5 clicked');
+    button5.style.backgroundColor = "#000080";
+    button5.textContent = "Freiburg";
+});
+button6 === null || button6 === void 0 ? void 0 : button6.addEventListener('click', function handleClick(event) {
+    console.log('button6 clicked');
+    button6.style.backgroundColor = "#000080";
+    button6.textContent = "Augsburg";
+});
+button7 === null || button7 === void 0 ? void 0 : button7.addEventListener('click', function handleClick(event) {
+    console.log('button7 clicked');
+    button7.style.backgroundColor = "#000080";
+    button7.textContent = "Bamberg";
+});
+button8 === null || button8 === void 0 ? void 0 : button8.addEventListener('click', function handleClick(event) {
+    console.log('button8 clicked');
+    button8.style.backgroundColor = "#000080";
+    button8.textContent = "Regensburg";
+});
+button9 === null || button9 === void 0 ? void 0 : button9.addEventListener('click', function handleClick(event) {
+    console.log('button9 clicked');
+    button9.style.backgroundColor = "#000080";
+    button9.textContent = "Essen";
+});
+button10 === null || button10 === void 0 ? void 0 : button10.addEventListener('click', function handleClick(event) {
+    console.log('button10 clicked');
+    button10.style.backgroundColor = "#000080";
+    button10.textContent = "Aachen";
+});
+button11 === null || button11 === void 0 ? void 0 : button11.addEventListener('click', function handleClick(event) {
+    console.log('button11 clicked');
+    button11.style.backgroundColor = "#000080";
+    button11.textContent = "Dortmund";
+});
+button12 === null || button12 === void 0 ? void 0 : button12.addEventListener('click', function handleClick(event) {
+    console.log('button12 clicked');
+    button12.style.backgroundColor = "#000080";
+    button12.textContent = "Hanover";
+});
+button13 === null || button13 === void 0 ? void 0 : button13.addEventListener('click', function handleClick(event) {
+    console.log('button13 clicked');
+    button13.style.backgroundColor = "#000080";
+    button13.textContent = "Bonn";
+});
+button14 === null || button14 === void 0 ? void 0 : button14.addEventListener('click', function handleClick(event) {
+    console.log('button14 clicked');
+    button14.style.backgroundColor = "#000080";
+    button14.textContent = "Heidelberg";
+});
+button15 === null || button15 === void 0 ? void 0 : button15.addEventListener('click', function handleClick(event) {
+    console.log('button15 clicked');
+    button15.style.backgroundColor = "#000080";
+    this.textContent = "Bremen";
+});
+button16 === null || button16 === void 0 ? void 0 : button16.addEventListener('click', function handleClick(event) {
+    console.log('button16 clicked');
+    button16.style.backgroundColor = "#000080";
+    button16.textContent = "Dresden";
+});
+button17 === null || button17 === void 0 ? void 0 : button17.addEventListener('click', function handleClick(event) {
+    console.log('button17 clicked');
+    button17.style.backgroundColor = "#000080";
+    button17.textContent = "Nuremberg";
+});
+button18 === null || button18 === void 0 ? void 0 : button18.addEventListener('click', function handleClick(event) {
+    console.log('button18 clicked');
+    button18.style.backgroundColor = "#000080";
+    button18.textContent = "Leipzig";
+});
+button19 === null || button19 === void 0 ? void 0 : button19.addEventListener('click', function handleClick(event) {
+    console.log('button19 clicked');
+    button19.style.backgroundColor = "#000080";
+    button19.textContent = "Düsseldorf";
+});
+button20 === null || button20 === void 0 ? void 0 : button20.addEventListener('click', function handleClick(event) {
+    console.log('button20 clicked');
+    button20.style.backgroundColor = "#000080";
+    button20.textContent = "Cologne";
+});
+button21 === null || button21 === void 0 ? void 0 : button21.addEventListener('click', function handleClick(event) {
+    console.log('button21 clicked');
+    button21.style.backgroundColor = "#000080";
+    button21.textContent = "Stuttgart";
+});
+button22 === null || button22 === void 0 ? void 0 : button22.addEventListener('click', function handleClick(event) {
+    console.log('button22 clicked');
+    button22.style.backgroundColor = "#000080";
+    button22.textContent = "Hamburg";
+});
+button23 === null || button23 === void 0 ? void 0 : button23.addEventListener('click', function handleClick(event) {
+    console.log('button23 clicked');
+    button23.style.backgroundColor = "#000080";
+    button23.textContent = "Munich";
+});
+button24 === null || button24 === void 0 ? void 0 : button24.addEventListener('click', function handleClick(event) {
+    console.log('button24 clicked');
+    button24.style.backgroundColor = "#000080";
+    button24.textContent = "Berlin";
+});
---
+++ src/advent.ts
@@ -0,0 +1,154 @@
+const button3 = document.getElementById('btn-3');
+const button4 = document.getElementById('btn-4');
+const button5 = document.getElementById('btn-5');
+const button6 = document.getElementById('btn-6');
+const button7 = document.getElementById('btn-7');
+const button8 = document.getElementById('btn-8');
+const button9 = document.getElementById('btn-9');
+const button10 = document.getElementById('btn-10');
+const button11 = document.getElementById('btn-11');
+const button12 = document.getElementById('btn-12');
+const button13 = document.getElementById('btn-13');
+const button14 = document.getElementById('btn-14');
+const button15 = document.getElementById('btn-15');
+const button16 = document.getElementById('btn-16');
+const button17 = document.getElementById('btn-17');
+const button18 = document.getElementById('btn-18');
+const button19 = document.getElementById('btn-19');
+const button20 = document.getElementById('btn-20');
+const button21 = document.getElementById('btn-21');
+const button22 = document.getElementById('btn-22');
+const button23 = document.getElementById('btn-23');
+const button24 = document.getElementById('btn-24');
+
+button3?.addEventListener('click', function handleClick(event) {
+    console.log('button3 clicked'); 
+    button3.style.backgroundColor = "#000080"
+    button3.textContent="Erfurt"     
+});
+
+button4?.addEventListener('click', function handleClick(event) {
+    console.log('button4 clicked');
+    button4.style.backgroundColor = "#000080"
+    button4.textContent="Bielefeld"
+});
+
+button5?.addEventListener('click', function handleClick(event) {
+    console.log('button5 clicked');
+    button5.style.backgroundColor = "#000080"
+    button5.textContent="Freiburg"
+});
+
+button6?.addEventListener('click', function handleClick(event) {
+    console.log('button6 clicked');
+    button6.style.backgroundColor = "#000080"
+    button6.textContent="Augsburg"
+});
+
+button7?.addEventListener('click', function handleClick(event) {
+    console.log('button7 clicked');
+    button7.style.backgroundColor = "#000080"
+    button7.textContent="Bamberg"
+});
+
+button8?.addEventListener('click', function handleClick(event) {
+    console.log('button8 clicked');
+    button8.style.backgroundColor = "#000080"
+    button8.textContent="Regensburg"
+});
+
+button9?.addEventListener('click', function handleClick(event) {
+    console.log('button9 clicked');
+    button9.style.backgroundColor = "#000080"
+    button9.textContent="Essen"
+});
+
+button10?.addEventListener('click', function handleClick(event) {
+    console.log('button10 clicked');
+    button10.style.backgroundColor = "#000080"
+    button10.textContent="Aachen"
+});
+
+button11?.addEventListener('click', function handleClick(event) {
+    console.log('button11 clicked');
+    button11.style.backgroundColor = "#000080"
+    button11.textContent="Dortmund"
+});
+
+button12?.addEventListener('click', function handleClick(event) {
+    console.log('button12 clicked');
+    button12.style.backgroundColor = "#000080"
+    button12.textContent="Hanover"
+});
+
+button13?.addEventListener('click', function handleClick(event) {
+    console.log('button13 clicked');
+    button13.style.backgroundColor = "#000080"
+    button13.textContent="Bonn"
+});
+
+button14?.addEventListener('click', function handleClick(event) {
+    console.log('button14 clicked');
+    button14.style.backgroundColor = "#000080"
+    button14.textContent="Heidelberg"
+});
+
+button15?.addEventListener('click', function handleClick(event) {
+    console.log('button15 clicked');
+    button15.style.backgroundColor = "#000080"
+    this.textContent="Bremen"
+});
+
+button16?.addEventListener('click', function handleClick(event) {
+    console.log('button16 clicked');
+    button16.style.backgroundColor = "#000080"
+    button16.textContent="Dresden"
+});
+
+button17?.addEventListener('click', function handleClick(event) {
+    console.log('button17 clicked');
+    button17.style.backgroundColor = "#000080"
+    button17.textContent="Nuremberg"
+});
+
+button18?.addEventListener('click', function handleClick(event) {
+    console.log('button18 clicked');
+    button18.style.backgroundColor = "#000080"
+    button18.textContent="Leipzig"
+});
+
+button19?.addEventListener('click', function handleClick(event) {
+    console.log('button19 clicked');
+    button19.style.backgroundColor = "#000080"
+    button19.textContent="Düsseldorf"
+});
+
+button20?.addEventListener('click', function handleClick(event) {
+    console.log('button20 clicked');
+    button20.style.backgroundColor = "#000080"
+    button20.textContent="Cologne"
+});
+
+button21?.addEventListener('click', function handleClick(event) {
+    console.log('button21 clicked');
+    button21.style.backgroundColor = "#000080"
+    button21.textContent="Stuttgart"
+});
+
+button22?.addEventListener('click', function handleClick(event) {
+    console.log('button22 clicked');
+    button22.style.backgroundColor = "#000080"
+    button22.textContent="Hamburg"
+});
+
+button23?.addEventListener('click', function handleClick(event) {
+    console.log('button23 clicked');
+    button23.style.backgroundColor = "#000080"
+    button23.textContent="Munich"
+});
+
+button24?.addEventListener('click', function handleClick(event) {
+    console.log('button24 clicked');
+    button24.style.backgroundColor = "#000080"
+    button24.textContent="Berlin"
+});
