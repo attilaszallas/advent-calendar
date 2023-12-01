c4a1946fb5311df9bb5b5b9f1fa3f149caff053a
Attila Szallas <ASzallas@slb.com>
2023-12-01 16:35:41 +0100

    Apply cookie

--- advent.js
+++ advent.js
@@ -20,113 +20,249 @@ var button21 = document.getElementById('btn-21');
 var button22 = document.getElementById('btn-22');
 var button23 = document.getElementById('btn-23');
 var button24 = document.getElementById('btn-24');
+// Setup state from cookies
 button3 === null || button3 === void 0 ? void 0 : button3.addEventListener('click', function handleClick(event) {
     console.log('button3 clicked');
     button3.style.backgroundColor = "#000080";
     button3.textContent = "Erfurt";
+    setCookie("btn3Opened", "true");
 });
 button4 === null || button4 === void 0 ? void 0 : button4.addEventListener('click', function handleClick(event) {
     console.log('button4 clicked');
     button4.style.backgroundColor = "#000080";
     button4.textContent = "Bielefeld";
+    setCookie("btn4Opened", "true");
 });
 button5 === null || button5 === void 0 ? void 0 : button5.addEventListener('click', function handleClick(event) {
     console.log('button5 clicked');
     button5.style.backgroundColor = "#000080";
     button5.textContent = "Freiburg";
+    setCookie("btn5Opened", "true");
 });
 button6 === null || button6 === void 0 ? void 0 : button6.addEventListener('click', function handleClick(event) {
     console.log('button6 clicked');
     button6.style.backgroundColor = "#000080";
     button6.textContent = "Augsburg";
+    setCookie("btn6Opened", "true");
 });
 button7 === null || button7 === void 0 ? void 0 : button7.addEventListener('click', function handleClick(event) {
     console.log('button7 clicked');
     button7.style.backgroundColor = "#000080";
     button7.textContent = "Bamberg";
+    setCookie("btn7Opened", "true");
 });
 button8 === null || button8 === void 0 ? void 0 : button8.addEventListener('click', function handleClick(event) {
     console.log('button8 clicked');
     button8.style.backgroundColor = "#000080";
     button8.textContent = "Regensburg";
+    setCookie("btn8Opened", "true");
 });
 button9 === null || button9 === void 0 ? void 0 : button9.addEventListener('click', function handleClick(event) {
     console.log('button9 clicked');
     button9.style.backgroundColor = "#000080";
     button9.textContent = "Essen";
+    setCookie("btn9Opened", "true");
 });
 button10 === null || button10 === void 0 ? void 0 : button10.addEventListener('click', function handleClick(event) {
     console.log('button10 clicked');
     button10.style.backgroundColor = "#000080";
     button10.textContent = "Aachen";
+    setCookie("btn10Opened", "true");
 });
 button11 === null || button11 === void 0 ? void 0 : button11.addEventListener('click', function handleClick(event) {
     console.log('button11 clicked');
     button11.style.backgroundColor = "#000080";
     button11.textContent = "Dortmund";
+    setCookie("btn11Opened", "true");
 });
 button12 === null || button12 === void 0 ? void 0 : button12.addEventListener('click', function handleClick(event) {
     console.log('button12 clicked');
     button12.style.backgroundColor = "#000080";
-    button12.textContent = "Hanover";
+    button12.textContent = "Hannover";
+    setCookie("btn12Opened", "true");
 });
 button13 === null || button13 === void 0 ? void 0 : button13.addEventListener('click', function handleClick(event) {
     console.log('button13 clicked');
     button13.style.backgroundColor = "#000080";
     button13.textContent = "Bonn";
+    setCookie("btn13Opened", "true");
 });
 button14 === null || button14 === void 0 ? void 0 : button14.addEventListener('click', function handleClick(event) {
     console.log('button14 clicked');
     button14.style.backgroundColor = "#000080";
     button14.textContent = "Heidelberg";
+    setCookie("btn14Opened", "true");
 });
 button15 === null || button15 === void 0 ? void 0 : button15.addEventListener('click', function handleClick(event) {
     console.log('button15 clicked');
     button15.style.backgroundColor = "#000080";
-    this.textContent = "Bremen";
+    button15.textContent = "Bremen";
+    setCookie("btn15Opened", "true");
 });
 button16 === null || button16 === void 0 ? void 0 : button16.addEventListener('click', function handleClick(event) {
     console.log('button16 clicked');
     button16.style.backgroundColor = "#000080";
     button16.textContent = "Dresden";
+    setCookie("btn16Opened", "true");
 });
 button17 === null || button17 === void 0 ? void 0 : button17.addEventListener('click', function handleClick(event) {
     console.log('button17 clicked');
     button17.style.backgroundColor = "#000080";
     button17.textContent = "Nuremberg";
+    setCookie("btn17Opened", "true");
 });
 button18 === null || button18 === void 0 ? void 0 : button18.addEventListener('click', function handleClick(event) {
     console.log('button18 clicked');
     button18.style.backgroundColor = "#000080";
     button18.textContent = "Leipzig";
+    setCookie("btn18Opened", "true");
 });
 button19 === null || button19 === void 0 ? void 0 : button19.addEventListener('click', function handleClick(event) {
     console.log('button19 clicked');
     button19.style.backgroundColor = "#000080";
     button19.textContent = "Düsseldorf";
+    setCookie("btn19Opened", "true");
 });
 button20 === null || button20 === void 0 ? void 0 : button20.addEventListener('click', function handleClick(event) {
     console.log('button20 clicked');
     button20.style.backgroundColor = "#000080";
     button20.textContent = "Cologne";
+    setCookie("btn20Opened", "true");
 });
 button21 === null || button21 === void 0 ? void 0 : button21.addEventListener('click', function handleClick(event) {
     console.log('button21 clicked');
     button21.style.backgroundColor = "#000080";
     button21.textContent = "Stuttgart";
+    setCookie("btn21Opened", "true");
 });
 button22 === null || button22 === void 0 ? void 0 : button22.addEventListener('click', function handleClick(event) {
     console.log('button22 clicked');
     button22.style.backgroundColor = "#000080";
     button22.textContent = "Hamburg";
+    setCookie("btn22Opened", "true");
 });
 button23 === null || button23 === void 0 ? void 0 : button23.addEventListener('click', function handleClick(event) {
     console.log('button23 clicked');
     button23.style.backgroundColor = "#000080";
     button23.textContent = "Munich";
+    setCookie("btn23Opened", "true");
 });
 button24 === null || button24 === void 0 ? void 0 : button24.addEventListener('click', function handleClick(event) {
     console.log('button24 clicked');
     button24.style.backgroundColor = "#000080";
     button24.textContent = "Berlin";
+    setCookie("btn24Opened", "true");
 });
+if (document.cookie.indexOf('btn3Opened=') + 1) {
+    button3.style.backgroundColor = "#000080";
+    button3.textContent = "Erfurt";
+}
+if (document.cookie.indexOf('btn4Opened=') + 1) {
+    button4.style.backgroundColor = "#000080";
+    button4.textContent = "Bielefeld";
+}
+if (document.cookie.indexOf('btn5Opened=') + 1) {
+    button5.style.backgroundColor = "#000080";
+    button5.textContent = "Freiburg";
+}
+if (document.cookie.indexOf('btn6Opened=') + 1) {
+    button6.style.backgroundColor = "#000080";
+    button6.textContent = "Augsburg";
+}
+if (document.cookie.indexOf('btn7Opened=') + 1) {
+    button7.style.backgroundColor = "#000080";
+    button7.textContent = "Bamberg";
+}
+if (document.cookie.indexOf('btn8Opened=') + 1) {
+    button8.style.backgroundColor = "#000080";
+    button8.textContent = "Regensburg";
+}
+if (document.cookie.indexOf('btn9Opened=') + 1) {
+    button9.style.backgroundColor = "#000080";
+    button9.textContent = "Essen";
+}
+if (document.cookie.indexOf('btn10Opened=') + 1) {
+    button10.style.backgroundColor = "#000080";
+    button10.textContent = "Aachen";
+}
+if (document.cookie.indexOf('btn11Opened=') + 1) {
+    button11.style.backgroundColor = "#000080";
+    button11.textContent = "Dortmund";
+}
+if (document.cookie.indexOf('btn12Opened=') + 1) {
+    button12.style.backgroundColor = "#000080";
+    button12.textContent = "Hannover";
+}
+if (document.cookie.indexOf('btn13Opened=') + 1) {
+    button13.style.backgroundColor = "#000080";
+    button13.textContent = "Bonn";
+}
+if (document.cookie.indexOf('btn14Opened=') + 1) {
+    button14.style.backgroundColor = "#000080";
+    button14.textContent = "Heidelberg";
+}
+if (document.cookie.indexOf('btn15Opened=') + 1) {
+    button15.style.backgroundColor = "#000080";
+    button15.textContent = "Bremen";
+}
+if (document.cookie.indexOf('btn16Opened=') + 1) {
+    button16.style.backgroundColor = "#000080";
+    button16.textContent = "Dresden";
+}
+if (document.cookie.indexOf('btn17Opened=') + 1) {
+    button17.style.backgroundColor = "#000080";
+    button17.textContent = "Nuremberg";
+}
+if (document.cookie.indexOf('btn18Opened=') + 1) {
+    button18.style.backgroundColor = "#000080";
+    button18.textContent = "Leipzig";
+}
+if (document.cookie.indexOf('btn19Opened=') + 1) {
+    button19.style.backgroundColor = "#000080";
+    button19.textContent = "Düsseldorf";
+}
+if (document.cookie.indexOf('btn20Opened=') + 1) {
+    button20.style.backgroundColor = "#000080";
+    button20.textContent = "Cologne";
+}
+if (document.cookie.indexOf('btn21Opened=') + 1) {
+    button21.style.backgroundColor = "#000080";
+    button21.textContent = "Stuttgart";
+}
+if (document.cookie.indexOf('btn22Opened=') + 1) {
+    button22.style.backgroundColor = "#000080";
+    button22.textContent = "Hamburg";
+}
+if (document.cookie.indexOf('btn23Opened=') + 1) {
+    button23.style.backgroundColor = "#000080";
+    button23.textContent = "Munich";
+}
+if (document.cookie.indexOf('btn24Opened=') + 1) {
+    button24.style.backgroundColor = "#000080";
+    button24.textContent = "Berlin";
+}
+/*
+ * General utils for managing cookies in Typescript.
+ */
+function setCookie(name, val) {
+    var date = new Date();
+    var value = val;
+    // Set it expire in 7 days
+    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+    // Set it
+    document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
+}
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) {
+        return parts.pop().split(";").shift();
+    }
+}
+function deleteCookie(name) {
+    var date = new Date();
+    // Set it expire in -1 days
+    date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+    // Set it
+    document.cookie = name + "=; expires=" + date.toUTCString() + "; path=/";
+}
--- src/advent.ts
+++ src/advent.ts
@@ -22,133 +22,320 @@ const button23 = document.getElementById('btn-23');
 const button24 = document.getElementById('btn-24');
 
 button3?.addEventListener('click', function handleClick(event) {
-    console.log('button3 clicked'); 
+    console.log('button3 clicked');
     button3.style.backgroundColor = "#000080"
-    button3.textContent="Erfurt"     
+    button3.textContent="Erfurt"  
+    setCookie("btn3Opened", "true")
 });
 
 button4?.addEventListener('click', function handleClick(event) {
     console.log('button4 clicked');
     button4.style.backgroundColor = "#000080"
     button4.textContent="Bielefeld"
+    setCookie("btn4Opened", "true")
 });
 
 button5?.addEventListener('click', function handleClick(event) {
     console.log('button5 clicked');
     button5.style.backgroundColor = "#000080"
     button5.textContent="Freiburg"
+    setCookie("btn5Opened", "true")
 });
 
 button6?.addEventListener('click', function handleClick(event) {
     console.log('button6 clicked');
     button6.style.backgroundColor = "#000080"
     button6.textContent="Augsburg"
+    setCookie("btn6Opened", "true")
 });
 
 button7?.addEventListener('click', function handleClick(event) {
     console.log('button7 clicked');
     button7.style.backgroundColor = "#000080"
     button7.textContent="Bamberg"
+    setCookie("btn7Opened", "true")
 });
 
 button8?.addEventListener('click', function handleClick(event) {
     console.log('button8 clicked');
     button8.style.backgroundColor = "#000080"
     button8.textContent="Regensburg"
+    setCookie("btn8Opened", "true")
 });
 
 button9?.addEventListener('click', function handleClick(event) {
     console.log('button9 clicked');
     button9.style.backgroundColor = "#000080"
     button9.textContent="Essen"
+    setCookie("btn9Opened", "true")
 });
 
 button10?.addEventListener('click', function handleClick(event) {
     console.log('button10 clicked');
     button10.style.backgroundColor = "#000080"
     button10.textContent="Aachen"
+    setCookie("btn10Opened", "true")
 });
 
 button11?.addEventListener('click', function handleClick(event) {
     console.log('button11 clicked');
     button11.style.backgroundColor = "#000080"
     button11.textContent="Dortmund"
+    setCookie("btn11Opened", "true")
 });
 
 button12?.addEventListener('click', function handleClick(event) {
     console.log('button12 clicked');
     button12.style.backgroundColor = "#000080"
-    button12.textContent="Hanover"
+    button12.textContent="Hannover"
+    setCookie("btn12Opened", "true")
 });
 
 button13?.addEventListener('click', function handleClick(event) {
     console.log('button13 clicked');
     button13.style.backgroundColor = "#000080"
     button13.textContent="Bonn"
+    setCookie("btn13Opened", "true")
 });
 
 button14?.addEventListener('click', function handleClick(event) {
     console.log('button14 clicked');
     button14.style.backgroundColor = "#000080"
     button14.textContent="Heidelberg"
+    setCookie("btn14Opened", "true")
 });
 
 button15?.addEventListener('click', function handleClick(event) {
     console.log('button15 clicked');
     button15.style.backgroundColor = "#000080"
-    this.textContent="Bremen"
+    button15.textContent="Bremen"
+    setCookie("btn15Opened", "true")
 });
 
 button16?.addEventListener('click', function handleClick(event) {
     console.log('button16 clicked');
     button16.style.backgroundColor = "#000080"
-    button16.textContent="Dresden"
+    button16.textContent="Dresden" 
+    setCookie("btn16Opened", "true")
 });
 
 button17?.addEventListener('click', function handleClick(event) {
     console.log('button17 clicked');
     button17.style.backgroundColor = "#000080"
     button17.textContent="Nuremberg"
+    setCookie("btn17Opened", "true")
 });
 
 button18?.addEventListener('click', function handleClick(event) {
     console.log('button18 clicked');
     button18.style.backgroundColor = "#000080"
     button18.textContent="Leipzig"
+    setCookie("btn18Opened", "true")
 });
 
 button19?.addEventListener('click', function handleClick(event) {
     console.log('button19 clicked');
     button19.style.backgroundColor = "#000080"
     button19.textContent="Düsseldorf"
+    setCookie("btn19Opened", "true")
 });
 
 button20?.addEventListener('click', function handleClick(event) {
     console.log('button20 clicked');
     button20.style.backgroundColor = "#000080"
     button20.textContent="Cologne"
+    setCookie("btn20Opened", "true")
 });
 
 button21?.addEventListener('click', function handleClick(event) {
     console.log('button21 clicked');
     button21.style.backgroundColor = "#000080"
     button21.textContent="Stuttgart"
+    setCookie("btn21Opened", "true")
 });
 
 button22?.addEventListener('click', function handleClick(event) {
     console.log('button22 clicked');
     button22.style.backgroundColor = "#000080"
     button22.textContent="Hamburg"
+    setCookie("btn22Opened", "true")
 });
 
 button23?.addEventListener('click', function handleClick(event) {
     console.log('button23 clicked');
     button23.style.backgroundColor = "#000080"
     button23.textContent="Munich"
+    setCookie("btn23Opened", "true")
 });
 
 button24?.addEventListener('click', function handleClick(event) {
     console.log('button24 clicked');
     button24.style.backgroundColor = "#000080"
     button24.textContent="Berlin"
+    setCookie("btn24Opened", "true")
 });
+
+if (document.cookie.indexOf('btn3Opened=') + 1)
+{
+    button3.style.backgroundColor = "#000080"
+    button3.textContent="Erfurt"     
+}
+
+if (document.cookie.indexOf('btn4Opened=') + 1)
+{
+    button4.style.backgroundColor = "#000080"
+    button4.textContent="Bielefeld"    
+}
+
+if (document.cookie.indexOf('btn5Opened=') + 1)
+{
+    button5.style.backgroundColor = "#000080"
+    button5.textContent="Freiburg"    
+}
+
+if (document.cookie.indexOf('btn6Opened=') + 1)
+{
+    button6.style.backgroundColor = "#000080"
+    button6.textContent="Augsburg"   
+}
+
+if (document.cookie.indexOf('btn7Opened=') + 1)
+{
+    button7.style.backgroundColor = "#000080"
+    button7.textContent="Bamberg"
+}
+
+if (document.cookie.indexOf('btn8Opened=') + 1)
+{
+    button8.style.backgroundColor = "#000080"
+    button8.textContent="Regensburg"
+}
+
+if (document.cookie.indexOf('btn9Opened=') + 1)
+{
+    button9.style.backgroundColor = "#000080"
+    button9.textContent="Essen"
+}
+
+if (document.cookie.indexOf('btn10Opened=') + 1)
+{
+    button10.style.backgroundColor = "#000080"
+    button10.textContent="Aachen"
+}
+
+if (document.cookie.indexOf('btn11Opened=') + 1)
+{
+    button11.style.backgroundColor = "#000080"
+    button11.textContent="Dortmund"
+}
+
+if (document.cookie.indexOf('btn12Opened=') + 1)
+{
+    button12.style.backgroundColor = "#000080"
+    button12.textContent="Hannover"
+}
+
+if (document.cookie.indexOf('btn13Opened=') + 1)
+{
+    button13.style.backgroundColor = "#000080"
+    button13.textContent="Bonn"
+}
+
+if (document.cookie.indexOf('btn14Opened=') + 1)
+{
+    button14.style.backgroundColor = "#000080"
+    button14.textContent="Heidelberg"
+}
+
+if (document.cookie.indexOf('btn15Opened=') + 1)
+{
+    button15.style.backgroundColor = "#000080"
+    button15.textContent="Bremen"
+}
+
+if (document.cookie.indexOf('btn16Opened=') + 1)
+{
+    button16.style.backgroundColor = "#000080"
+    button16.textContent="Dresden"
+}
+
+if (document.cookie.indexOf('btn17Opened=') + 1)
+{
+    button17.style.backgroundColor = "#000080"
+    button17.textContent="Nuremberg"
+}
+
+if (document.cookie.indexOf('btn18Opened=') + 1)
+{
+    button18.style.backgroundColor = "#000080"
+    button18.textContent="Leipzig"
+}
+
+if (document.cookie.indexOf('btn19Opened=') + 1)
+{
+    button19.style.backgroundColor = "#000080"
+    button19.textContent="Düsseldorf"
+}
+
+if (document.cookie.indexOf('btn20Opened=') + 1)
+{
+    button20.style.backgroundColor = "#000080"
+    button20.textContent="Cologne"
+}
+
+if (document.cookie.indexOf('btn21Opened=') + 1)
+{
+    button21.style.backgroundColor = "#000080"
+    button21.textContent="Stuttgart"
+}
+
+if (document.cookie.indexOf('btn22Opened=') + 1)
+{
+    button22.style.backgroundColor = "#000080"
+    button22.textContent="Hamburg"
+}
+
+if (document.cookie.indexOf('btn23Opened=') + 1)
+{
+    button23.style.backgroundColor = "#000080"
+    button23.textContent="Munich"
+}
+
+if (document.cookie.indexOf('btn24Opened=') + 1)
+{
+    button24.style.backgroundColor = "#000080"
+    button24.textContent="Berlin"
+}
+
+/*
+ * General utils for managing cookies in Typescript.
+ */
+function setCookie(name: string, val: string) {
+    const date = new Date();
+    const value = val;
+
+    // Set it expire in 7 days
+    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+
+    // Set it
+    document.cookie = name+"="+value+"; expires="+date.toUTCString()+"; path=/";
+}
+
+function getCookie(name: string) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    
+    if (parts.length == 2) {
+        return parts.pop().split(";").shift();
+    }
+}
+
+function deleteCookie(name: string) {
+    const date = new Date();
+
+    // Set it expire in -1 days
+    date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+
+    // Set it
+    document.cookie = name+"=; expires="+date.toUTCString()+"; path=/";
+}
